dfa7c62f999224b822c6a5e3e8bfa3e51a2943a7
uninicol <nicol.buratti@studenti.unicam.it>
2025-11-19 15:48:34 +0100

    implemented sql server

---
+++ nam.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace nam.Server.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+
+        public DbSet<Entities.User> Users{ get; set; }
+    }
+
+}
---
+++ nam.Server/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nam.Server.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(255)]
+        public required string Email{ get; set; }
+
+        [Required]
+        public required string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
