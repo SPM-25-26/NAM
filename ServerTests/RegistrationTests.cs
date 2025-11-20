fc112b8a7559cfecc40e2300ea7b7bf57ed0d5c7
uninicol <nicol.buratti@studenti.unicam.it>
2025-11-20 16:22:15 +0100

    init tests

---
+++ ServerTests/RegistrationTests.cs
@@ -0,0 +1,24 @@
+﻿namespace ServerTests
+{
+    [TestClass]
+    public sealed class RegistrationTests
+    {
+        [TestMethod]
+        public void RegisterUser_ReturnsOk_WhenRegistrationIsSuccessful()
+        {
+            return;
+        }
+
+        [TestMethod]
+        public void RegisterUser_ReturnsValidationProblem_WhenEmailOrPasswordAreIncorrect()
+        {
+            return;
+        }
+
+        [TestMethod]
+        public void RegisterUser_ReturnsBadRequest_WhenEmailAlreadyExists()
+        {
+            return;
+        }
+    }
+}
