1f9e65aefb70f33fef537f8237da5c11af4ef214
uninicol <nicol.buratti@studenti.unicam.it>
2025-11-21 12:48:50 +0100

    working registration test

--- ServerTests/RegistrationTests.cs
+++ ServerTests/RegistrationTests.cs
@@ -1,24 +1,51 @@
-﻿namespace ServerTests
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using nam.Server.Data;
+using nam.Server.Endpoints;
+using nam.Server.Models.DTOs;
+using nam.Server.Models.Validators;
+
+namespace nam.ServerTests
 {
     [TestClass]
     public sealed class RegistrationTests
     {
-        [TestMethod]
-        public void RegisterUser_ReturnsOk_WhenRegistrationIsSuccessful()
+        private ApplicationDbContext context;
+
+        [TestInitialize]
+        public void Setup()
         {
-            return;
+            // This creates the server in-memory
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDb_{Guid.NewGuid()}")
+                .Options;
+
+            // 2. Create the Context
+            context = new ApplicationDbContext(options);
         }
 
         [TestMethod]
-        public void RegisterUser_ReturnsValidationProblem_WhenEmailOrPasswordAreIncorrect()
+        public async Task RegisterUser_ReturnsOk_WhenRegistrationIsSuccessfulAsync()
         {
-            return;
-        }
+            // Arrange
+            RegisterUserValidator validator = new();
+            RegisterUserDto registrationData = new()
+            {
+                Email = "validmail@gmail.com",
+                Password = "ValidPassword123!",
+                ConfirmPassword = "ValidPassword123!"
+            };
 
-        [TestMethod]
-        public void RegisterUser_ReturnsBadRequest_WhenEmailAlreadyExists()
-        {
-            return;
+            var result = await AuthEndpoints.RegisterUser(registrationData, context, validator);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(Ok<string>));
+
+            var userInDb = await context.Users.FirstOrDefaultAsync(u => u.Email == registrationData.Email);
+
+            Assert.IsNotNull(userInDb, "User should exist in the database");
+            Assert.AreEqual("validmail@gmail.com", userInDb.Email);
         }
     }
 }
+
