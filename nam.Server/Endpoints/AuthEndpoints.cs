1d17d1902665ea190252a40fce0e8491b88c8ebb
Matteo <matteo.brachetta@studenti.unicam.it>
2025-11-24 19:38:57 +0100

    feat(auth): add reset password endpoint

--- nam.Server/Endpoints/AuthEndpoints.cs
+++ nam.Server/Endpoints/AuthEndpoints.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using nam.Server.Data;
 using nam.Server.Models.DTOs;
 using nam.Server.Models.Entities;
+using nam.Server.Services;
 using System;
 
 namespace nam.Server.Endpoints
@@ -41,6 +42,109 @@ namespace nam.Server.Endpoints
             await context.SaveChangesAsync();
 
             return TypedResults.Ok("User registered successfully");
+        }
+        
+        public static async Task<IResult> RequestPasswordReset(
+            [FromBody] PasswordResetRequestDto request,
+            ApplicationDbContext context,
+            IEmailService emailService,
+            ICodeService codeService
+            )
+        {
+            Console.WriteLine($"\n[DEBUG] Input DTO Ricevuto:\n{request}\n");
+            // Find a user
+            var user = await context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+
+            // Check find a user
+            if (user == null){
+                var notFoundResponse = new PasswordResetResponseDto
+                {
+                    Success = false, 
+                    Message = "The email not found"
+                };
+                return TypedResults.NotFound(notFoundResponse); 
+            }
+
+            // Generate Auth code
+            var authCode = codeService.GenerateAuthCode();
+            Console.WriteLine($"\n[DEBUG] auth generated:\n{authCode}\n");
+
+            // Build a response
+            var resetCode = new PasswordResetCode
+            {
+                UserId = user.Id.ToString(),
+                AuthCode = authCode,
+                CreatedAt = DateTime.UtcNow,
+                ExpiresAt = DateTime.UtcNow.AddMinutes(codeService.TimeToLiveMinutes) 
+            };
+            Console.WriteLine($"\n[DEBUG] reset code:\n{resetCode}\n");
+            
+            // Save reset code in dedicated table
+            context.ResetPasswordAuth.Add(resetCode);
+            await context.SaveChangesAsync();
+
+            // Send email
+            await emailService.SendEmailAsync(user.Email, "Reset code", $" AuthCode: {authCode} \n expired: {resetCode.ExpiresAt}");
+            
+            var response = new PasswordResetResponseDto
+            {
+                Success = true, 
+                Message = "Please check your email; we have sent you the reset code."
+            };
+            return TypedResults.Ok(response);
+        }
+
+        public static async Task<IResult> ResetPassword(
+            [FromBody] PasswordResetConfirmDto request,
+            ApplicationDbContext context)
+        {
+            // Find and validation reset code
+            var resetCode = await context.ResetPasswordAuth
+                .FirstOrDefaultAsync(c =>
+                    c.AuthCode == request.AuthCode &&
+                    c.ExpiresAt > DateTime.UtcNow);
+            
+            Console.WriteLine($"\n[DEBUG] check reset code:\n{resetCode}\n");
+            // Verify the validity of the reset code
+            if (resetCode == null){
+                 Console.WriteLine($"\n[DEBUG] reset code not found");
+                var notFoundResponse = new PasswordResetResponseDto
+                {
+                    Success = false, 
+                    Message = "Invalid or expired auth code."
+                };
+                return TypedResults.BadRequest(notFoundResponse);
+            }
+
+            // Find the user
+            var user = await context.Users
+                .FirstOrDefaultAsync(u => u.Id.ToString() == resetCode.UserId);
+            Console.WriteLine($"\n[DEBUG] check user {user}");
+
+            if (user == null){
+                 Console.WriteLine($"\n[DEBUG] user not found");
+                var notFoundUserResponse = new PasswordResetResponseDto
+                {
+                    Success = false, 
+                    Message = "Invalid auth code."
+                };
+                return TypedResults.BadRequest(new { Message =  notFoundUserResponse});
+            }
+
+           
+            //TODO: replace with service for encrypt data
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.NewPassword);
+
+            Console.WriteLine($"\n[DEBUG] password hashed: {user.PasswordHash}");
+           // Delete resetCode
+            context.ResetPasswordAuth.Remove(resetCode);
+
+            await context.SaveChangesAsync();
+
+            return TypedResults.Ok(new {
+                Success = true, 
+                Message = "Password successfully reset." 
+                });
         }
     }
 }
--- nam.Server/Endpoints/AuthMap.cs
+++ nam.Server/Endpoints/AuthMap.cs
@@ -16,6 +16,29 @@
                     return op;
                 });
 
+                groupBuilder.MapPost("/request-password-reset", AuthEndpoints.RequestPasswordReset)
+                .Produces(StatusCodes.Status200OK) 
+                .Produces(StatusCodes.Status400BadRequest)
+                .Produces(StatusCodes.Status404NotFound)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .WithOpenApi(op =>
+                {
+                    op.Summary = "Requests a password reset code for a given email address.";
+                    op.Description = "Sends a 6-digit code to the user's email if the account exists.";
+                    return op;
+                });
+
+                groupBuilder.MapPost("/password-reset", AuthEndpoints.ResetPassword)
+                .Produces(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status400BadRequest)
+                .Produces(StatusCodes.Status500InternalServerError)
+                .WithOpenApi(op =>
+                {
+                    op.Summary = "Resets the user's password using the Auth Code sent to their email.";
+                    op.Description = "Requires the user's email, the Auth Code, and the new password.";
+                    return op;
+                });
+
             return builder;
         }
     }
