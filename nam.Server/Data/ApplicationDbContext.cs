ef8f308b97c5e43ff90acca069705908fa2456bf
Matteo <matteo.brachetta@studenti.unicam.it>
2025-11-24 19:40:50 +0100

    feat(db): add PasswordResetCode entity and migration

--- nam.Server/Data/ApplicationDbContext.cs
+++ nam.Server/Data/ApplicationDbContext.cs
@@ -10,5 +10,6 @@ namespace nam.Server.Data
         }
 
         public DbSet<User> Users{ get; set; }
+        public DbSet<PasswordResetCode> ResetPasswordAuth{ get; set; }
     }
 }
--- nam.Server/Migrations/20251124182051_ResetPasswordAuth.Designer.cs
+++ nam.Server/Migrations/20251124182051_ResetPasswordAuth.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using nam.Server.Data;
 
@@ -11,9 +12,11 @@ using nam.Server.Data;
 namespace nam.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251124182051_ResetPasswordAuth")]
+    partial class ResetPasswordAuth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
