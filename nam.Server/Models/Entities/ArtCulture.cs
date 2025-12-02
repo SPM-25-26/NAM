2f11c15907906c72a23c532620b66b3dc8a0b529
nicol-buratti <nickibrt8@gmail.com>
2025-12-02 16:02:31 +0100

    fixed artculture

--- nam.Server/Models/Entities/ArtCulture.cs
+++ nam.Server/Models/Entities/ArtCulture.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace nam.Server.Models.Entities
 {
-    public class ArtCulture
+    public class ArtCultureNatureCard
     {
         [Key]
         [Required]
@@ -13,20 +13,22 @@ namespace nam.Server.Models.Entities
         [MaxLength(500)]
         public string EntityName { get; set; } = string.Empty;
 
+        [Required]
         [MaxLength(1000)]
         public string ImagePath { get; set; } = string.Empty;
 
+        [Required]
         [MaxLength(100)]
         public string BadgeText { get; set; } = string.Empty;
 
         [MaxLength(1000)]
         public string Address { get; set; } = string.Empty;
 
-        public ArtCultureDetail? Detail { get; set; }
+        public ArtCultureNatureDetail? Detail { get; set; }
     }
 
 
-    public class ArtCultureDetail
+    public class ArtCultureNatureDetail
     {
         [Key]
         [Required]
@@ -73,254 +75,189 @@ namespace nam.Server.Models.Entities
 
         public double Longitude { get; set; }
 
-        // Navigation properties per le relazioni con le entità figlie
-        public ICollection<Service> Services { get; set; } = new List<Service>();
+        public ICollection<CulturalSiteService> Services { get; set; } = new List<CulturalSiteService>();
         public ICollection<CulturalProject> CulturalProjects { get; set; } = new List<CulturalProject>();
         public ICollection<Catalogue> Catalogues { get; set; } = new List<Catalogue>();
-        public ICollection<CreativeWork> CreativeWorks { get; set; } = new List<CreativeWork>();
-        public ICollection<GalleryImage> Gallery { get; set; } = new List<GalleryImage>();
-        public ICollection<VirtualTour> VirtualTours { get; set; } = new List<VirtualTour>();
-        public ICollection<Neighbor> Neighbors { get; set; } = new List<Neighbor>();
+        public ICollection<CreativeWorkMobile> CreativeWorks { get; set; } = new List<CreativeWorkMobile>();
+        public ICollection<String> Gallery { get; set; } = new List<String>();
+        public ICollection<String> VirtualTours { get; set; } = new List<String>();
+        public ICollection<FeatureCard> Neighbors { get; set; } = new List<FeatureCard>();
         public ICollection<AssociatedService> AssociatedServices { get; set; } = new List<AssociatedService>();
 
-        // Relazioni one-to-one
         public NearestCarPark? NearestCarPark { get; set; }
-        public Site? Site { get; set; }
-        public MunicipalityData? MunicipalityData { get; set; }
+        public SiteCard? Site { get; set; }
+        public MunicipalityForLocalStorageSetting? MunicipalityData { get; set; }
     }
 
-    public class Service
+    public class CulturalSiteService
     {
         [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
 
         [Required]
         [MaxLength(255)]
-        public string Name { get; set; } = string.Empty;
+        public required string Name { get; set; }
 
-        [MaxLength(2000)]
-        public string Description { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(255)]
-        public string ArtCultureDetailId { get; set; } = string.Empty;
-
-        [ForeignKey(nameof(ArtCultureDetailId))]
-        public ArtCultureDetail ArtCultureDetail { get; set; } = null!;
+        [MaxLength(1000)]
+        public string? Description { get; set; }
     }
 
     public class CulturalProject
     {
         [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
 
         [Required]
         [MaxLength(255)]
-        public string Name { get; set; } = string.Empty;
-
-        [MaxLength(1000)]
-        public string Url { get; set; } = string.Empty;
+        public required string Name { get; set; }
 
         [Required]
-        [MaxLength(255)]
-        public string ArtCultureDetailId { get; set; } = string.Empty;
-
-        [ForeignKey(nameof(ArtCultureDetailId))]
-        public ArtCultureDetail ArtCultureDetail { get; set; } = null!;
+        [MaxLength(2048)]
+        [Url]
+        public required string Url { get; set; }
     }
 
     public class Catalogue
     {
         [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
 
         [Required]
         [MaxLength(255)]
-        public string Name { get; set; } = string.Empty;
+        public required string Name { get; set; }
 
-        [MaxLength(1000)]
-        public string WebsiteUrl { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(2048)]
+        [Url]
+        public required string WebsiteUrl { get; set; }
 
         [MaxLength(2000)]
-        public string Description { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(255)]
-        public string ArtCultureDetailId { get; set; } = string.Empty;
-
-        [ForeignKey(nameof(ArtCultureDetailId))]
-        public ArtCultureDetail ArtCultureDetail { get; set; } = null!;
+        public string? Description { get; set; }
     }
 
-    public class CreativeWork
+    public class CreativeWorkMobile
     {
         [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
 
         [Required]
         [MaxLength(100)]
-        public string Type { get; set; } = string.Empty;
-
-        [MaxLength(1000)]
-        public string Url { get; set; } = string.Empty;
+        public required string Type { get; set; }
 
         [Required]
-        [MaxLength(255)]
-        public string ArtCultureDetailId { get; set; } = string.Empty;
-
-        [ForeignKey(nameof(ArtCultureDetailId))]
-        public ArtCultureDetail ArtCultureDetail { get; set; } = null!;
+        [MaxLength(2048)]
+        [Url]
+        public required string Url { get; set; }
     }
 
-    public class GalleryImage
+    public class FeatureCard
     {
         [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        [Required]
-        [MaxLength(1000)]
-        public string ImagePath { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(255)]
-        public string ArtCultureDetailId { get; set; } = string.Empty;
-
-        [ForeignKey(nameof(ArtCultureDetailId))]
-        public ArtCultureDetail ArtCultureDetail { get; set; } = null!;
-    }
-
-    public class VirtualTour
-    {
-        [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        [Required]
-        [MaxLength(1000)]
-        public string TourUrl { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(255)]
-        public string ArtCultureDetailId { get; set; } = string.Empty;
-
-        [ForeignKey(nameof(ArtCultureDetailId))]
-        public ArtCultureDetail ArtCultureDetail { get; set; } = null!;
-    }
-
-    public class Neighbor
-    {
-        [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        [Required]
-        [MaxLength(255)]
-        public string EntityId { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(255)]
-        public string Title { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(50)]
-        public string Category { get; set; } = string.Empty;
-
-        [MaxLength(1000)]
-        public string ImagePath { get; set; } = string.Empty;
-
-        [MaxLength(500)]
-        public string ExtraInfo { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(255)]
-        public string ArtCultureDetailId { get; set; } = string.Empty;
-
-        [ForeignKey(nameof(ArtCultureDetailId))]
-        public ArtCultureDetail ArtCultureDetail { get; set; } = null!;
-    }
-
-    public class NearestCarPark
-    {
-        [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        public double Latitude { get; set; }
-
-        public double Longitude { get; set; }
-
-        [MaxLength(1000)]
-        public string Address { get; set; } = string.Empty;
-
-        public double Distance { get; set; }
-    }
-
-    public class Site
-    {
-        [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
-
-        [Required]
-        [MaxLength(255)]
-        public string Identifier { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(500)]
-        public string OfficialName { get; set; } = string.Empty;
-
-        [MaxLength(1000)]
-        public string ImagePath { get; set; } = string.Empty;
-
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
         [MaxLength(100)]
-        public string Category { get; set; } = string.Empty;
+        public string? EntityId { get; set; }
 
-        [Required]
         [MaxLength(255)]
-        public string ArtCultureDetailId { get; set; } = string.Empty;
+        public string? Title { get; set; }
 
-        [ForeignKey(nameof(ArtCultureDetailId))]
-        public ArtCultureDetail ArtCultureDetail { get; set; } = null!;
-    }
+        public MobileCategory Category { get; set; }
 
-    public class MunicipalityData
-    {
-        [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
+        [MaxLength(500)]
+        public string? ImagePath { get; set; }
 
-        [Required]
-        [MaxLength(255)]
-        public string Name { get; set; } = string.Empty;
-
-        [MaxLength(1000)]
-        public string LogoPath { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(255)]
-        public string ArtCultureDetailId { get; set; } = string.Empty;
-
-        [ForeignKey(nameof(ArtCultureDetailId))]
-        public ArtCultureDetail ArtCultureDetail { get; set; } = null!;
+        public string? ExtraInfo { get; set; }
     }
 
     public class AssociatedService
     {
         [Key]
-        public Guid Id { get; set; } = Guid.NewGuid();
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public required string Identifier { get; set; }
 
         [Required]
         [MaxLength(255)]
-        public string Identifier { get; set; } = string.Empty;
+        public required string Name { get; set; }
 
         [Required]
-        [MaxLength(255)]
-        public string Name { get; set; } = string.Empty;
-
-        [MaxLength(1000)]
-        public string ImagePath { get; set; } = string.Empty;
-
-        [Required]
-        [MaxLength(255)]
-        public string ArtCultureDetailId { get; set; } = string.Empty;
-
-        [ForeignKey(nameof(ArtCultureDetailId))]
-        public ArtCultureDetail ArtCultureDetail { get; set; } = null!;
+        [MaxLength(500)]
+        public required string ImagePath { get; set; }
     }
 
+    public class NearestCarPark
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        public double Latitude { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
+
+        [MaxLength(500)]
+        public string? Address { get; set; }
+
+        public double Distance { get; set; }
+    }
+
+    public class SiteCard
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public required string Identifier { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public required string OfficialName { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public required string ImagePath { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string Category { get; set; }
+    }
+
+    public enum MobileCategory
+    {
+        Sleep,
+        EatAndDrink,
+        Events,
+        ArtCulture,
+        Nature,
+        TypicalProducts,
+        Routes,
+        Services,
+        EntertainmentLeisure,
+        Organizations,
+        Articles,
+        Shopping
+    }
+
+    public class MobileCategoryDetail
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        public MobileCategory Category { get; set; }
+
+        [MaxLength(500)]
+        public string? ImagePath { get; set; }
+
+        [MaxLength(100)]
+        public string? Label { get; set; }
+    }
 }
