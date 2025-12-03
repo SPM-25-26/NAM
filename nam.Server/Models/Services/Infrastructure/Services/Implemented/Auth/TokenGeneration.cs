6629f778c307466dd74770d8a47bac7b312356cc
nicol-buratti <nickibrt8@gmail.com>
2025-12-03 16:40:08 +0100

    moved file to the right directory

--- nam.Server/Models/Services/Infrastructure/Services/Implemented/Auth/TokenGeneration.cs
+++ nam.Server/Models/Services/Infrastructure/Services/Implemented/Auth/TokenGeneration.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using nam.Server.Models.Options;
+using nam.Server.Models.Services.Infrastructure.Services.Interfaces.Auth;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace nam.Server.Models.Services.Infrastructure.Services.Interfaces.Auth
+namespace nam.Server.Models.Services.Infrastructure.Services.Implemented.Auth
 {
     public class TokenGeneration(IOptionsMonitor<JwtOptions> jwtOptions) : ITokenGeneration
     {
