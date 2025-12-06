4b038f554dd4619c1d4b486958b54a2b42cbb4d1
nicol-buratti <nickibrt8@gmail.com>
2025-12-06 12:38:42 +0100

    fixed imports of tests

--- ServerTests/mock/AuthServiceTestBuilder.cs
+++ ServerTests/mock/AuthServiceTestBuilder.cs
@@ -2,8 +2,10 @@
 using Microsoft.Extensions.Configuration;
 using nam.Server.Data;
 using nam.Server.Models.Entities;
-using nam.Server.Models.Services.Infrastructure;
-using nam.Server.Models.Services.Infrastructure.Repositories;
+using nam.Server.Models.Services.Infrastructure.Repositories.Interfaces;
+using nam.Server.Models.Services.Infrastructure.Services.Implemented.Auth;
+using nam.Server.Models.Services.Infrastructure.Services.Interfaces;
+using nam.Server.Models.Services.Infrastructure.Services.Interfaces.Auth;
 using System.Linq.Expressions;
 using System.Security.Claims;
 
