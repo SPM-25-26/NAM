b4c55e38a924625f7e8135e8af433ef0704b51b0
nicol-buratti <nickibrt8@gmail.com>
2026-01-03 21:46:14 +0100

    added comments to injection core

--- DataInjection.Core/Interfaces/IProvider.cs
+++ DataInjection.Core/Interfaces/IProvider.cs
@@ -1,7 +1,18 @@
 ﻿namespace DataInjection.Interfaces
 {
+    /// <summary>
+    /// Defines a contract for providing an entity of type <typeparamref name="TEntity"/>.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity to provide.</typeparam>
     public interface IProvider<TEntity>
     {
+        /// <summary>
+        /// Asynchronously retrieves an entity.
+        /// </summary>
+        /// <param name="ct">A cancellation token to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation. The task result contains the retrieved entity.
+        /// </returns>
         Task<TEntity> GetEntity(CancellationToken ct = default);
     }
 }
--- DataInjection.Core/Interfaces/ISyncService.cs
+++ DataInjection.Core/Interfaces/ISyncService.cs
@@ -1,7 +1,18 @@
 ﻿namespace DataInjection.Interfaces
 {
+    /// <summary>
+    /// Defines a contract for a synchronization service that executes synchronization logic for entities.
+    /// </summary>
     public interface ISyncService
     {
+        /// <summary>
+        /// Asynchronously executes the synchronization process for the specified entity type using the provided entity collector.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity to synchronize.</typeparam>
+        /// <param name="entityCollector">The entity collector used to retrieve entities for synchronization.</param>
+        /// <returns>
+        /// A task that represents the asynchronous synchronization operation.
+        /// </returns>
         Task ExecuteSyncAsync<TEntity>(IEntityCollector<TEntity> entityCollector);
     }
 }
